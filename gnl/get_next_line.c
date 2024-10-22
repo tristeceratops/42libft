5d0ce66cf941d01bc043bea6116800821da188a5
tristeceratops <eric.woillard@gmail.com>
2024-10-22 17:28:00 +0200

    gnl update

--- gnl/get_next_line.c
+++ gnl/get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: ewoillar <ewoillar@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/04 16:01:59 by ewoillar          #+#    #+#             */
-/*   Updated: 2024/10/22 17:09:57 by ewoillar         ###   ########.fr       */
+/*   Updated: 2024/10/22 17:27:40 by ewoillar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,53 +24,60 @@ char	*get_line_null(char **remainder, char **line)
 
 char	*loop(char *remainder[1024], char *buffer, int fd)
 {
-	int	bytes_read;
+    int	bytes_read;
 
-	bytes_read = 1;
-	while (!has_newline(remainder[fd]) && bytes_read > 0)
-	{
-		bytes_read = read(fd, buffer, BUFFER_SIZE);
-		if (bytes_read < 0)
-		{
-			free(buffer);
-			return (NULL);
-		}
-		buffer[bytes_read] = '\0';
-		printf("buffer: %s\n", buffer);
-		printf("remainder: %s\n", remainder[fd]);
-		printf("bytes_read: %d\n", bytes_read);
-		remainder[fd] = ft_strjoin(remainder[fd], buffer);
-	}
-	if (bytes_read == 0 && !remainder[fd])
-	{
-		free(buffer);
-		return (NULL);
-	}
-	return (buffer);
+    bytes_read = 1;
+    while (!has_newline(remainder[fd]) && bytes_read > 0)
+    {
+        bytes_read = read(fd, buffer, BUFFER_SIZE);
+        if (bytes_read < 0)
+        {
+            free(buffer);
+            return (NULL);
+        }
+        buffer[bytes_read] = '\0';
+        printf("buffer: %s\n", buffer);
+        printf("remainder: %s\n", remainder[fd]);
+        printf("bytes_read: %d\n", bytes_read);
+        remainder[fd] = ft_strjoin(remainder[fd], buffer);
+        if (!remainder[fd])
+        {
+            free(buffer);
+            return (NULL);
+        }
+    }
+    if (bytes_read == 0 && (!remainder[fd] || !*remainder[fd]))
+    {
+        free(buffer);
+        return (NULL);
+    }
+    return (buffer);
 }
 
 char	*get_next_line(int fd)
 {
-	static char	*remainder[1024];
-	char		*buffer;
-	char		*line;
+    static char	*remainder[1024];
+    char		*buffer;
+    char		*line;
 
-	if (fd < 0 || fd >= 1024)
-		return (NULL);
-	buffer = malloc((BUFFER_SIZE + 1) * sizeof(char));
-	if (!buffer)
-		return (NULL);
-	if (!remainder[fd])
-		remainder[fd] = NULL;
-	buffer = loop(remainder, buffer, fd);
-	if (buffer == NULL)
-	{
-		free(buffer);
-		return (NULL);
-	}
-	free(buffer);
-	line = get_line(&remainder[fd]);
-	return (line);
+    if (fd < 0 || fd >= 1024)
+        return (NULL);
+    buffer = malloc((BUFFER_SIZE + 1) * sizeof(char));
+    if (!buffer)
+        return (NULL);
+    if (!remainder[fd])
+        remainder[fd] = ft_strdup(""); // Initialize remainder[fd] if NULL
+    printf("Initial remainder[fd]: %s\n", remainder[fd]); // Debugging
+    buffer = loop(remainder, buffer, fd);
+    if (buffer == NULL)
+    {
+        free(buffer);
+        return (NULL);
+    }
+    free(buffer);
+    line = get_line(&remainder[fd]);
+    printf("Final line: %s\n", line); // Debugging
+    return (line);
 }
 /*
 int	main(void)
